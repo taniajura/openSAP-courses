fd7b12aeaddc2253f050e5e0405ff76e9b606f9a
taniajura <erick.taniajura@gmail.com>
2016-05-28 05:47:35 +0000

    Initial sync from Web IDE

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,1 @@
+sap.ui.define([	"sap/ui/core/mvc/Controller"], function (Controller) {	"use strict";	return Controller.extend("opensap.myapp.controller.App", {		onShowHello : function () {			// show a native JavaScript alert			alert("Hello World");		}	});});
